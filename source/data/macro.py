eb7bff387f990b2110e0d7211884c2e52f6f1d0c
昵称有六个字 <1931960436@qq.com>
2023-09-26 17:22:23 +0800

    Update macro.py
    add RFRData Class

--- source/data/macro.py
+++ source/data/macro.py
@@ -2,7 +2,7 @@
 # @Author: 昵称有六个字
 # @Date:   2023-09-25 21:03:07
 # @Last Modified by:   昵称有六个字
-# @Last Modified time: 2023-09-26 14:24:29
+# @Last Modified time: 2023-09-26 15:26:12
 """GDPData().df_gdp"""
 
 
@@ -65,6 +65,39 @@ class GDPData(object):
         self.df_gdp = self.df_gdp[self.df_gdp["year"] >= Setting.sample_start_year]
 
 
+class RFRData(object):
+    """
+    Risk-free rate data preprocess
+    --------
+
+    Args:
+    --------
+        file_name (str, optional): Risk-free rate data file name. Defaults to "rfr.csv".
+        columns (list[str], optional): Risk-free rate data columns. \n
+        Defaults to ["year", "month", "rfr"]
+
+    Usages:
+    --------
+        ```python
+        import pandas as pd
+        df_rfr: pd.DataFrame = RFRData().df_rfr
+        print(df_rfr)
+        ```
+    """
+
+    def __init__(
+        self, file_name: str = "rfr.csv", columns: list[str] = ["year", "month", "rfr"]
+    ) -> None:
+        # Read risk-free rate data
+        self.df_rfr: pd.DataFrame = pd.read_csv(f"{Setting.macro_path}/{file_name}")
+        # Reset columns
+        self.df_rfr.columns = columns
+        # Type convert
+        self.df_rfr = self.df_rfr.astype({"year": int, "month": int, "rfr": float})
+
+
 if __name__ == "__main__":
-    df_gdp = GDPData().df_gdp
-    ic(df_gdp)
+    # df_gdp = GDPData().df_gdp
+    # ic(df_gdp)
+    df_rfr = RFRData().df_rfr
+    ic(df_rfr)
