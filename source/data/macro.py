3f1b1b395361609069662efcfc5e41cc679eeb58
昵称有六个字 <1931960436@qq.com>
2023-09-28 19:01:10 +0800

    Inflation Data

--- source/data/macro.py
+++ source/data/macro.py
@@ -2,8 +2,12 @@
 # @Author: 昵称有六个字
 # @Date:   2023-09-25 21:03:07
 # @Last Modified by:   昵称有六个字
-# @Last Modified time: 2023-09-26 15:26:12
-"""GDPData().df_gdp"""
+# @Last Modified time: 2023-09-28 19:00:22
+"""
+GDPData().df_gdp
+RFRData().df_rfr
+InflationData().df_inflation
+"""
 
 
 import sys
@@ -64,7 +68,7 @@ class GDPData(object):
         # Set start year
         self.df_gdp = self.df_gdp[self.df_gdp["year"] >= Setting.sample_start_year]
 
-
+@singleton
 class RFRData(object):
     """
     Risk-free rate data preprocess
@@ -94,10 +98,60 @@ class RFRData(object):
         self.df_rfr.columns = columns
         # Type convert
         self.df_rfr = self.df_rfr.astype({"year": int, "month": int, "rfr": float})
+        # Set start year
+        self.df_rfr = self.df_rfr[self.df_rfr["year"] >= Setting.sample_start_year]
+
+@singleton
+class InflationData(object):
+    """
+    Inflation data preprocess
+    ------
+
+    Args:
+    --------
+        file_name (str, optional): Inflation data file name. Defaults to "inflation.csv".
+        columns (list[str], optional): Inflation data columns. \n
+        Defaults to ["year", "quarter", "CPI", "PPI"]
+
+    Usages:
+    --------
+        ```python
+        import pandas as pd
+        df_inflation: pd.DataFrame = InflationData().df_inflation
+        print(df_inflation)
+        ```
+    """
+
+    def __init__(
+        self,
+        file_name: str = "inflation.csv",
+        columns: list[str] = ["year", "quarter", "CPI", "PPI"],
+    ) -> None:
+        # Import CPI & PPI data
+        self.df_inflation: pd.DataFrame = pd.read_csv(
+            f"{Setting.macro_path}/{file_name}"
+        )
+        # Reset columns
+        self.df_inflation.columns = columns
+        # Convert data type
+        self.df_inflation = self.df_inflation.astype(
+            {
+                "year": int,
+                "quarter": int,
+                "CPI": float,
+                "PPI": float,
+            }
+        )
+        # Set start year
+        self.df_inflation = self.df_inflation[
+            self.df_inflation["year"] >= Setting.sample_start_year
+        ]
 
 
 if __name__ == "__main__":
     # df_gdp = GDPData().df_gdp
     # ic(df_gdp)
-    df_rfr = RFRData().df_rfr
-    ic(df_rfr)
+    # df_rfr = RFRData().df_rfr
+    # ic(df_rfr)
+    df_inflation = InflationData().df_inflation
+    ic(df_inflation)
